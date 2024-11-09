d90be4c7d683991326155692b9d29e731836120f
Alex-apostolo <alex-apostolo@hotmail.com>
2024-11-09 23:00:05 +0200

    Added support for excel

--- src/file_text_extractor.py
+++ src/file_text_extractor.py
@@ -2,7 +2,7 @@ from PIL import Image
 from pdf2image import convert_from_bytes
 from pytesseract import image_to_string
 from docx import Document
-
+import pandas as pd
 
 def extract_text(file_stream, file_extension):
     if file_extension == ".pdf":
@@ -29,6 +29,16 @@ def extract_text(file_stream, file_extension):
         # Read text file from byte stream
         file_stream.seek(0)
         return file_stream.read().decode("utf-8")
+
+    elif file_extension in [".xls", ".xlsx"]:
+        # Read Excel file from byte stream
+        file_stream.seek(0)
+        excel_data = pd.read_excel(file_stream, sheet_name=None)  # Read all sheets
+        text = ""
+        for sheet_name, sheet_data in excel_data.items():
+            text += f"\nSheet: {sheet_name}\n"
+            text += sheet_data.to_string(index=False, header=True)  # Convert sheet to string
+        return text
 
     else:
         raise ValueError(f"Unsupported file extension: {file_extension}")
